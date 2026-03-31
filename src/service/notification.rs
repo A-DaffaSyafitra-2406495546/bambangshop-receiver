298d30a23857ff23930f67a614d7747b3604a2b9
syafitr4 <daffasyafitra12@gmail.com>
2026-03-31 20:26:28 +0700

    Implement subscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -8,4 +8,44 @@ use crate::repository::notification::NotificationRepository;
 pub struct NotificationService;
 
 impl NotificationService {
+    #[tokio::main]
+    async fn subscribe_request(product_type: String) -> Result<SubscriberRequest> {
+        let product_type_upper: String = product_type.to_uppercase();
+        let notification_receiver_url: String = format!("{}/receive", APP_CONFIG.get_instance_root_url());
+
+        let payload: SubscriberRequest = SubscriberRequest {
+            name: APP_CONFIG.get_instance_name().to_string(),
+            url: notification_receiver_url.clone(),
+        };
+
+        let request_url: String = format!("{}/notification/subscribe/{}", APP_CONFIG.get_publisher_root_url(), product_type_upper);
+        let request = REQWEST_CLIENT
+            .post(&request_url)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .json(&payload)
+            .send()
+            .await;
+
+        match request {
+            Ok(response) => match response.json::<SubscriberRequest>().await {
+                Ok(k) => Ok(k),
+                Err(e) => Err(compose_error_response(
+                    Status::NotAcceptable,
+                    e.to_string()
+                )),
+            },
+            Err(e) => Err(compose_error_response(
+                Status::NotFound,
+                e.to_string()
+            )),
+        }
+    }
+
+    pub fn subscribe(product_type: &str) -> Result<SubscriberRequest> {
+        let product_type_clone: String = String::from(product_type);
+        return thread::spawn(move || Self::subscribe_request(product_type_clone))
+            .join()
+            .unwrap();
+    }
 }
